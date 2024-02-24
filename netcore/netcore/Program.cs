aff66a3434ace85ae1d1b40823403b604a0557d4
hudingwen <765472804@qq.com>
2024-02-24 14:55:30 +0800

    fix

--- netcore/netcore/Program.cs
+++ netcore/netcore/Program.cs
@@ -4,18 +4,29 @@ using System.Reflection.PortableExecutable;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
- 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+                      });
+});
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
 //配置管理
 ConfigHelper.Configuration = builder.Configuration;
 
 var app = builder.Build();
 
+app.UseCors(MyAllowSpecificOrigins);
 //开启静态文件访问
 app.UseStaticFiles();
 // Configure the HTTP request pipeline.
